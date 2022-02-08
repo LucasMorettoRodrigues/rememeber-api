1a69324912685bb28dd342a48163c46eb00f0712
Lucas <r.lucasmoretto@gmail.com>
2022-02-08 15:41:00 -0300

    develop update/delete projects

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,23 +1,60 @@
 const Project = require('../models/Project')
 
 const getAllProjects = async (req, res) => {
-    res.send('get all projects')
+    const projects = await Project.find({ createdBy: req.user.userId })
+    res.status(200).json({ projects })
 }
 
 const createProject = async (req, res) => {
-    res.send('create project')
+    req.body.createdBy = req.user.userId
+    const project = await Project.create( req.body )
+    res.status(201).json({ project })
 }
 
 const getProject = async (req, res) => {
-    res.send('get single project')
+    const project = await Project.findOne({ 
+            createdBy: req.user.userId,
+            _id: req.params.id
+        })
+
+    if(!project) {
+        throw new Error('Project not found')
+    }
+
+    res.status(200).json({ project })
 }
 
 const updateProject = async (req, res) => {
-    res.send('update project')
+    const { name } = req.body
+
+    if(name === '') {
+        throw new Error('Name field is empity')
+    }
+
+    const project = await Project.findByIdAndUpdate(
+        { _id: req.params.id, createdBy: req.user.userId, },
+        req.body,
+        { new: true, runValidators: true}
+    )
+
+    if(!project) {
+        throw new Error('Project not found')
+    }
+
+    res.status(200).json({ project })
 }
 
 const deleteProject = async (req, res) => {
-    res.send('delete project')
+    const project = await Project.findByIdAndRemove({ 
+        createdBy: req.user.userId,
+        _id: req.params.id
+    })
+
+    if(!project) {
+        throw new Error('Project not found')
+    }
+
+    res.status(200).send()
 }
 
 module.exports = {
